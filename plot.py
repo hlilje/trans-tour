a94d905f83dab4ef4021658f0c287212259ca349
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-02-06 09:49:55 +0100

    Add function for plotting addresses

--- plot.py
+++ plot.py
@@ -21,11 +21,18 @@ def plot_addresses(start_addr, addrs):
     """
     Plot the given start address and addresses on the map.
     """
-    # lon, lat, optional height
-    kml.newpoint(name="Kirstenbosch", coords=[(18.432314,-33.988862)])
+    # Lon, lat, optional height
+    start_addr = start_addr[0] # Unpack tuple from array
+    kml.newpoint(name=start_addr[0] + ' ' + start_addr[1],
+            coords=[(start_addr[4], start_addr[5])])
+
+    # Plot all addresses
+    for addr in addrs:
+        kml.newpoint(name=addr[0], coords=[(addr[3], addr[4])])
 
 def save():
     """
     Save the kml map file.
     """
     kml.save(FILE_NAME)
+    print("Saved file:", FILE_NAME)
